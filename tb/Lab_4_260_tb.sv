1a2fb4620178f7eb5fc09c6d3efc84b95f0b67c8
Aba <abarajithan-7@gmail.com>
2023-06-14 15:56:55 -0700

    Add self checking

--- tb/Lab_4_260_tb.sv
+++ tb/Lab_4_260_tb.sv
@@ -1,34 +1,61 @@
-// Lab5b_tb	  
-// testbench for programmable message encryption
-// CSE140L  Spring 2020   
-// Pick a starting sequence;  
-// Run lab 4 equivalent to encrypt
-`include "uvm_macros.svh"
-import uvm_pkg::*;
+// `include "uvm_macros.svh"
+// import uvm_pkg::*;
 
-typedef logic [7:0] encrypt_t [64];
+typedef logic [7:0] unpacked_8_64 [64];
 
-function encrypt_t encrypt (
-    input string str2,
-    input logic [7:0] pre_length,
+function unpacked_8_64 pad (
+  input string str2,
+  input logic [7:0] pre_length
+  );
+
+    int lk, ct;
+
+    // set preamble lengths for the program runs (always > 6)
+    // ***** choose any value > 6 *****
+    
+    if(pre_length < 7) begin
+      $display("illegal preamble length chosen, overriding with 8");
+      pre_length =  8;                     // override < 6 with a legal value
+    end else
+      $display("preamble length = %d",pre_length);
+
+    if(str2.len>50) 
+      $display("illegally long string of length %d, truncating to 50 chars.",str2.len);
+
+    $display("original message string length = %d",str2.len);
+
+    for(lk = 0; lk<str2.len; lk++)
+      if(str2[lk]==8'h5f) continue;	       // count leading _ chars in string
+	  else break;                          // we shall add these to preamble pad length
+	  $display("embedded leading underscore count = %d",lk);
+
+    for(int nn=0; nn<64; nn++)			   // count leading underscores
+      if(str2[nn]==8'h5f) ct++; 
+	  else break;
+	  $display("ct = %d",ct);
+
+    $display("run encryption of this original message: ");
+    $display("%s",str2);             // print original message in transcript window
+
+    for(int j=0; j<64; j++) 			   // pre-fill message_padded with ASCII _ characters
+      pad[j] = 8'h5f;         
+    for(int l=0; l<str2.len; l++)  	 // overwrite up to 60 of these spaces w/ message itself
+      pad[pre_length+l] = byte'(str2[l]); 
+
+endfunction
+
+function unpacked_8_64 encrypt (
+    input unpacked_8_64 msg_padded2,
     input int pat_sel,
     input logic [5:0] LFSR_init  // for program 2 run
   );
 
-    int lk, ct;
-    int str_len;
     string      str_enc2[64];          // decryption program input
     logic [5:0] LFSR_ptrn[6];		       // 6 possible maximal-length 6-bit LFSR tap ptrns
-    logic [7:0] msg_padded2[64], 
-                msg_crypto2[64];
+    unpacked_8_64 msg_crypto2;
     logic [5:0] lfsr_ptrn,
                 lfsr2[64];
 
-    str_len = str2.len;
-    if(str_len>50) 
-      $display("illegally long string of length %d, truncating to 50 chars.",str_len);
-    //    for(int ml=50; ml<64; ml++)
-    //      str2[ml] = 8'h5f;
     // the 6 possible (constant) maximal-length feedback tap patterns from which to choose
     LFSR_ptrn[0] = 6'h21;
     LFSR_ptrn[1] = 6'h2D;
@@ -36,15 +63,6 @@ function encrypt_t encrypt (
     LFSR_ptrn[3] = 6'h33;
     LFSR_ptrn[4] = 6'h36;
     LFSR_ptrn[5] = 6'h39;
-    // set preamble lengths for the program runs (always > 6)
-    // ***** choose any value > 6 *****
-    
-    if(pre_length < 7) begin
-      $display("illegal preamble length chosen, overriding with 8");
-      pre_length =  8;                     // override < 6 with a legal value
-    end  
-    else
-      $display("preamble length = %d",pre_length);
 
     // select LFSR tap pattern
     // ***** choose any value < 6 *****
@@ -64,32 +82,19 @@ function encrypt_t encrypt (
     end
     else
       $display("LFSR starting pattern = %b",LFSR_init);
-    $display("original message string length = %d",str_len);
-    for(lk = 0; lk<str_len; lk++)
-      if(str2[lk]==8'h5f) continue;	       // count leading _ chars in string
-	  else break;                          // we shall add these to preamble pad length
-    
-	  $display("embedded leading underscore count = %d",lk);
+
     // precompute encrypted message
 	  lfsr_ptrn = LFSR_ptrn[pat_sel];        // select one of the 6 permitted tap ptrns
-    // write the three control settings into data_memory of DUT
 
 	  lfsr2[0]     = LFSR_init;              // any nonzero value (zero may be helpful for debug)
-    $display("run encryption of this original message: ");
-    $display("%s",str2);                   // print original message in transcript window
     $display();
     $display("LFSR_ptrn = %h, LFSR_init = %h %h",lfsr_ptrn,LFSR_init,lfsr2[0]);
-    for(int j=0; j<64; j++) 			   // pre-fill message_padded with ASCII _ characters
-      msg_padded2[j] = 8'h5f;         
-    for(int l=0; l<str_len; l++)  		   // overwrite up to 60 of these spaces w/ message itself
-	  msg_padded2[pre_length+l] = byte'(str2[l]); 
 
     // compute the LFSR sequence
     for (int ii=0;ii<63;ii++) begin :lfsr_loop
       lfsr2[ii+1] = (lfsr2[ii]<<1)+(^(lfsr2[ii]&lfsr_ptrn));//{LFSR[6:0],(^LFSR[5:3]^LFSR[7])};		   // roll the rolling code
     //      $display("lfsr_ptrn %d = %h",ii,lfsr2[ii]);
     end	  :lfsr_loop
-
 
     // encrypt the message
     for (int i=0; i<64; i++) begin		   // testbench will change on falling clocks
@@ -112,11 +117,6 @@ function encrypt_t encrypt (
       $write(" %h",msg_padded2[jj]);
 	  $display("\n");
 
-    for(int nn=0; nn<64; nn++)			   // count leading underscores
-      if(str2[nn]==8'h5f) ct++; 
-	  else break;
-	  $display("ct = %d",ct);
-
     return msg_crypto2;
 
 endfunction
@@ -130,13 +130,13 @@ module Lab_4_260_tb             ;
               data_in           ;
   wire  [7:0] data_out          ;
   wire        done              ;          // done flag returned by DUT
-  logic [7:0] pre_length        ,          // bytes before first character in message
-              msg_crypto2[64]   ,          // encrypted message according to the DUT
+  logic [7:0] pre_length        ;          // bytes before first character in message
+	logic [5:0] LFSR_init         ;      
+  logic [7:0] msg_crypto2[64]   ,          // encrypted message according to the DUT
               msg_decryp2[64]   ,          // recovered decrypted message from DUT
-			        LFSR_init         ;      
+              msg_padded2[64]   ;
 
   string      str2;
-  int         str_len           ;		       // length of string (character count)
   string      str_dec2[64]      ;          // decrypted string will go here
   int pat_sel                   ;          // LFSR pattern select
 
@@ -150,12 +150,12 @@ module Lab_4_260_tb             ;
 //    $fgets(str2, fi);
 
     str2 = "Mr_Watson_come_here_I_want_to_see_you";//_my_aide";
-    str_len = str2.len;
     pre_length = 10;    // values 7 to 63 recommended
     pat_sel =  2;
     LFSR_init = 6'h01;  // for program 2 run
-
-    msg_crypto2 = encrypt(.str2(str2), .pre_length(pre_length), .pat_sel(pat_sel), .LFSR_init(LFSR_init));
+    
+    msg_padded2 = pad(.str2(str2), .pre_length(pre_length));
+    msg_crypto2 = encrypt(.msg_padded2(msg_padded2), .pat_sel(pat_sel), .LFSR_init(LFSR_init));
 
     // run decryption program 
     repeat(5) @(posedge clk);
@@ -176,24 +176,30 @@ module Lab_4_260_tb             ;
     #10ns $display("done at time %t",$time);
     $display("run decryption:");
 
-    for(int n=0; n<str_len+1; n++) begin
+    for(int n=0; n<str2.len+1; n++) begin
       @(posedge clk);
       raddr          <= n;
       @(posedge clk);
       msg_decryp2[n] <= data_out;
     end
 
-    for(int rr=0; rr<str_len+1; rr++)
+    for(int rr=0; rr<str2.len+1; rr++)
       str_dec2[rr] = string'(msg_decryp2[rr]);
 
     @(posedge clk)
-    for(int qq=0; qq<str_len+1; qq++)
+    for(int qq=0; qq<str2.len+1; qq++)
       $writeh(msg_decryp2[qq]);
     $display();
 
-    for(int ss=0; ss<str_len+1; ss++)
+    for(int ss=0; ss<str2.len+1; ss++)
       $write("%s",str_dec2[ss]);
-    $display();    
+    $display();
+
+    assert (msg_padded2 == msg_padded2) 
+      $display ("Decryption successful");
+    else
+      $display ("Decryption failed");
+
 
     #20ns $stop;
   end  :initial_loop
